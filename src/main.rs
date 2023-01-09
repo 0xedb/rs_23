1d48c2ff18a3b2be048c7cff6c095cb3920eb05f
0xedb <edohbruno@gmail.com>
2023-01-09 13:25:53 +0000

    c13

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,5 @@
-trait Empty {
-    fn say(&self) {
-        println!("Hi!")
-    }
-}
-
-#[derive(Debug)]
-struct Something;
-
-impl Empty for Something {}
-
-fn f(t: &impl Empty) {
-    t.say()
-}
-
-fn shout() -> &'static str {
-    "shouting!"
-}
-
-fn exp<'a>(f: &'a str) {
-    println!("{f}")
-}
-
 fn main() {
-    let r#strict = "so strict";
-    let s = Something;
+    let mirror = |x| x;
 
-    exp("dkafj");
-
-    f(&s);
-    s.say();
-    println!("{strict}");
-
-    let b = b'H';
-    let bb = b"hello world ";
-    let aa = r#"hello!"#;
-
-    println!("{bb:?} {aa} {b:?}");
-
-    let a = "foo";
-    let b = "fo\
-    o";
-    let c = "fo\
-
-
-    o";
-
-    println!("{} {} {}", a == b, b == c, c == a);
-
-    // let l16 = 390348u8;
-
-
+    println!("{}", mirror(200));
 }
