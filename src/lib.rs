345a288f986e780856ac3057e91dc086249088c1
0xedb <edohbruno@gmail.com>
2023-01-10 02:12:28 +0000

    c14

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+/*!
+just something to thing about here!!!!
+*/
+
+
 pub fn t() -> i32 {
     0xEDB
 }
@@ -8,4 +13,15 @@ mod tests {
     fn if_it_works() {
         assert!(super::t() == 3803);
     }
+}
+
+
+/// just a function called a
+/// # Example
+/// ```
+/// let res = rs_23::a("hello");
+/// assert_eq!("hello", res);
+/// ```
+pub fn a(p: &str) -> &str {
+    p
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,30 @@ fn main() {
     let mirror = |x| x;
 
     println!("{}", mirror(200));
+
+    let one_to_ten = 1..=10;
+
+    let l: Vec<_> = one_to_ten.map(|x| x * x).collect();
+
+    let a = [54; 10];
+
+    println!("{l:?} {a:?}");
+
+    let play = |x| println!("{x} -- {a:?}");
+
+    play(342);
+    println!("{a:?}");
+
+    let c: char = Default::default();
+    println!("{c:?}");
+}
+
+/// just a function called a
+/// # Example
+/// ```
+/// let res = a("hello");
+/// assert_eq!("hello", res);
+/// ```
+fn a(p: &str) -> &str {
+    p
 }
