05fa17d8898d22e41b65e8de8a1ee3bbfd06c1ce
0xedb <edohbruno@gmail.com>
2023-01-12 02:43:18 +0000

    c15

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,35 @@
-fn main() {
-    let mirror = |x| x;
+#[derive(Debug)]
+struct Mine<T>(T);
 
-    println!("{}", mirror(200));
+impl<T> std::ops::Deref for Mine<T> {
+    type Target = T;
 
-    let one_to_ten = 1..=10;
-
-    let l: Vec<_> = one_to_ten.map(|x| x * x).collect();
-
-    let a = [54; 10];
-
-    println!("{l:?} {a:?}");
-
-    let play = |x| println!("{x} -- {a:?}");
-
-    play(342);
-    println!("{a:?}");
-
-    let c: char = Default::default();
-    println!("{c:?}");
-
-    let  n = &mut 42342;
-    *n = 0xedb;
-
-    println!("{n}")
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
-/// just a function called a
-/// # Example
-/// ```
-/// let res = a("hello");
-/// assert_eq!("hello", res);
-/// ```
-fn a(p: &str) -> &str {
-    p
+impl<T> Drop for Mine<T> {
+    fn drop(&mut self) {
+        println!("you got serve!!!");
+    }
+}
+
+fn main() {
+    // let mine = Mine(300);
+
+    use std::rc::Rc;
+
+    let r = Rc::new(Mine(2000));
+
+    let p = vec![Rc::clone(&r)];
+    println!("count {}", Rc::strong_count(&r));
+    drop(p);
+    let w = vec![Rc::clone(&r)];
+
+    println!("count {}", Rc::strong_count(&r));
+
+    // drop(mine);
+
+    // println!("yours {mine:?}");
 }
