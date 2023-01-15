b82b73cd40f5ec7da1eb094199c2668ade10030a
0xedb <edohbruno@gmail.com>
2023-01-15 22:29:33 +0000

    c21

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,18 @@
-trait Serial {
-    fn serialize(&self);
+// #[derive(PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-#[derive(Debug)]
-struct HTML;
-
-#[derive(Debug)]
-struct Svelte;
-
-#[derive(Debug)]
-struct React;
-
-fn serilize_it(s: Vec<Box<dyn Serial>>) {
-    s.iter().for_each(|a| a.serialize())
+impl std::cmp::PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x
+    }
 }
-
-fn ss(s: &dyn Serial) {}
-
-// fn bb(s: Serial) {}
-
-unsafe fn i_can_byte() {
-    println!("byting you!");
-}
-
-static 
 
 fn main() {
-    for i in 0..10 {
-        println!("{i}")
-    }
+    let one = Point { x: 0, y: 1 };
+    let two = Point { x: 0, y: 2 };
 
-    let mut p = 3493;
-
-    let mut v = &mut p as *mut i32;
-
-    unsafe {
-        *v = 0xedb;
-        println!("{}", *v);
-    }
-
-    unsafe {
-        i_can_byte();
-    }
+    println!("{}", one == two);
 }
