557713a55b24651b535b294b3bc436c51e541611
0xedb <edohbruno@gmail.com>
2023-01-28 01:22:38 +0000

    custom iterator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
 // vecs & slices
 
+struct Mine {
+    pos: usize,
+    message: String,
+}
+
+impl Mine {
+    fn new(message: String) -> Self {
+        Self {
+            pos: Default::default(),
+            message,
+        }
+    }
+}
+
+impl Iterator for Mine {
+    type Item = char;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos < 10 {
+            let p = self.pos;
+            self.pos += 1;
+            Some(self.message.chars().nth(p).unwrap())
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
-    let items = [1, 2, 3, 4];
+    let mine = Mine::new("2023 Hello World News".to_string());
+
+    for i in mine {
+        println!("{i}");
+    }
 }
