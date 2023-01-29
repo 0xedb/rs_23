ca1c26aeb500055bab62bb1b2a1d98903b5e1d74
0xedb <edohbruno@gmail.com>
2023-01-29 23:22:04 +0000

    await

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use futures::executor::block_on;
 
+async fn say_my_name() {
+    println!("yes sir, saying your name");
+}
+
 async fn hello_world() {
+    say_my_name().await;
     println!("hello world");
 }
 
