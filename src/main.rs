96f5746ed914074a57dbe4a8484681bab860e54d
0xedb <edohbruno@gmail.com>
2023-01-30 02:44:11 +0000

    with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use futures::executor::block_on;
+// use futures::executor::block_on;
 
-async fn say_my_name() {
-    println!("yes sir, saying your name");
+// async fn say_my_name() {
+//     println!("yes sir, saying your name");
+// }
+
+// async fn hello_world() {
+//     say_my_name().await;
+//     println!("hello world");
+// }
+
+// fn main() {
+//     let hw = hello_world();
+
+//     block_on(hw);
+// }
+
+async fn for_tokio() {
+    println!("Welcome to Tokio");
 }
 
-async fn hello_world() {
-    say_my_name().await;
-    println!("hello world");
-}
+#[tokio::main]
+async fn main() {
+    let ans = for_tokio();
 
-fn main() {
-    let hw = hello_world();
-
-    block_on(hw);
+    ans.await;
 }
