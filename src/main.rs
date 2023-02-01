43cf24b874409528b1ecaedb5843dc74f384e35b
0xedb <edohbruno@gmail.com>
2023-02-01 16:58:38 +0000

    macro rules by example

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,76 @@
-// use futures::executor::block_on;
+// // use futures::executor::block_on;
 
-// async fn say_my_name() {
-//     println!("yes sir, saying your name");
+// // async fn say_my_name() {
+// //     println!("yes sir, saying your name");
+// // }
+
+// // async fn hello_world() {
+// //     say_my_name().await;
+// //     println!("hello world");
+// // }
+
+// // fn main() {
+// //     let hw = hello_world();
+
+// //     block_on(hw);
+// // }
+
+// use std::ops::Not;
+
+// #[derive(Debug, Default)]
+// struct Mine {
+//     something: Option<i32>,
 // }
 
-// async fn hello_world() {
-//     say_my_name().await;
-//     println!("hello world");
+// impl Not for Mine {
+//     type Output = Self;
+//     fn not(self) -> Self::Output {
+//         Mine {
+//             ..Default::default()
+//         }
+//     }
 // }
 
-// fn main() {
-//     let hw = hello_world();
+// use tokio::task;
 
-//     block_on(hw);
+// #[inline]
+// async fn for_tokio() -> &'static str {
+//     "funny right?"
 // }
 
-use std::ops::Not;
+// #[tokio::main]
+// async fn main() {
+//     let ans = for_tokio();
 
-#[derive(Debug, Default)]
-struct Mine {
-    something: Option<i32>,
-}
+//     println!("{}", ans.await);
 
-impl Not for Mine {
-    type Output = Self;
-    fn not(self) -> Self::Output {
-        Mine {
-            ..Default::default()
-        }
+//     let a = 300;
+
+//     task::spawn(async move {
+//         println!("inside tokio thread {a}");
+//     })
+//     .await
+//     .unwrap();
+
+//     println!("from main thread");
+
+//     let mine = Mine{something: Some(0xedb)};
+//     print!("{mine:?}");
+//     let n_mine = !mine;
+
+
+//     print!("{n_mine:?}");
+// }
+
+
+macro_rules! magic_answer {
+    () => {println!("INCORRECT")};
+    ("open") => {
+        println!("CORRECT")
     }
 }
 
-use tokio::task;
-
-#[inline]
-async fn for_tokio() -> &'static str {
-    "funny right?"
-}
-
-#[tokio::main]
-async fn main() {
-    let ans = for_tokio();
-
-    println!("{}", ans.await);
-
-    let a = 300;
-
-    task::spawn(async move {
-        println!("inside tokio thread {a}");
-    })
-    .await
-    .unwrap();
-
-    println!("from main thread");
-
-    let mine = Mine{something: Some(0xedb)};
-    print!("{mine:?}");
-    let n_mine = !mine;
-
-
-    print!("{n_mine:?}");
+fn main() {
+    magic_answer!();
+    magic_answer!("open");
 }
