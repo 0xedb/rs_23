3b3824c1ca38b16e345cad4b9cbc3f4312c2f97a
0xedb <edohbruno@gmail.com>
2023-02-08 14:23:50 +0000

    server examples

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{
+    cookie, get,
+    http::{header, StatusCode},
+    middleware, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use std::{
     net::Ipv4Addr,
     sync::{Arc, Mutex},
     time::Duration,
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct ServCounter {
-    count: Arc<Mutex<i64>>,
+    count: Mutex<i64>,
 }
 
 #[get("/")]
-async fn index(data: web::Data<ServCounter>) -> impl Responder {
-    let mut d = data.count.lock().unwrap();
-
-    *d += 1;
-
-    let ans = format!("{}", *d);
-
-    ans
+async fn index(req: HttpRequest) -> impl Responder {
+    HttpResponse::build(StatusCode::ACCEPTED)
+        .cookie(cookie::Cookie::new("sample", "value"))
+        .body("hello")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = ServCounter {
-        count: Arc::new(Mutex::new(0x0)),
-    };
+    let counter = web::Data::new(ServCounter {
+        count: Mutex::new(0xedb),
+    });
 
     HttpServer::new(move || {
+        // println!("hjeeradfdaf");
         App::new()
+            .wrap(middleware::Logger::default())
             .service(index)
-            .app_data(web::Data::new(counter.clone()))
+            .app_data(counter.clone())
+            .app_data(39usize)
     })
     .bind((Ipv4Addr::new(127, 0, 0, 1), 2023))?
     .keep_alive(Duration::from_secs(20))
