af34a1af3ec8471d164e85d1779eb43b81cfe34e
0xedb <edohbruno@gmail.com>
2023-02-17 22:25:19 +0000

    server

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
- 
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "hello world"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new())
+        .bind("127.0.0.1:2023")?
+        .run()
+        .await
+}
